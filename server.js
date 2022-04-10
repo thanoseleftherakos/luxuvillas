0c87ef4a515c36322a67c0347dc7ac3e34f3adec
Thanos Eleftherakos <teleftherakos@gmail.com>
2022-04-10 17:45:53 +0300

    express fix

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-// const express = require("express");
-// const serveStatic = require("serve-static");
-// const history = require("connect-history-api-fallback");
-
-// const path = require("path");
-// app = express();
-// app.use(serveStatic(path.join(__dirname, "dist")));
-// app.use(history());
-// const port = process.env.PORT || 3000;
-// app.listen(port);
-
 const express = require("express");
-const path = require("path");
 const history = require("connect-history-api-fallback");
-// ^ middleware to redirect all URLs to index.html
-
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname, "dist"));
-
-app.use(staticFileMiddleware);
 app.use(history());
-app.use(staticFileMiddleware);
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-
-app.get("/", function (req, res) {
-  res.render(path.join(__dirname + "/index.html"));
+app.use(express.static("dist"));
+app.get("/", (req, res) => {
+  res.sendFile("dist/index.html");
 });
-
-app.listen(3000);
+app.listen(3000, () => console.log("server started"));
